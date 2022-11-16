5ea2306a89efe2d57979c735e056578b131a074d
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2022-11-16 21:24:37 +0530

    one-to-one select query generation works

--- src/scribe/core.clj
+++ src/scribe/core.clj
@@ -1,6 +1,183 @@
-(ns scribe.core)
+(ns scribe.core
+  (:require [clojure.string :as str]
+            [clojure.walk :as walk]
+            [scribe.postgres :as pg]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+;; useful for writing macros
+(defn concat-symbols
+  "Concatenates all the symbols passed."
+  [& syms]
+  (->> syms
+       (apply str)
+       symbol))
+
+
+(defmacro declare-model
+  "Like declare but for records. 
+  declare creates vars and you can't redefine a var as a record."
+  [& models]
+  (cons 'do
+        (for [m models]
+          `(defrecord ~m []))))
+
+
+;; protocol that all models will implement
+
+(defprotocol SModel
+  (spec [this]))
+
+(defn spec-name [name]
+  (concat-symbols name '-spec))
+
+(defmacro defmodel [name {:keys [record] :as spec*}]
+  (let [fields (->> record
+                    keys
+                    (mapv symbol))]
+    `(do (defn ~(spec-name name) [] ~spec*)
+         (defrecord ~name ~fields
+           SModel
+           (~'spec [~'this] ~spec*)))))
+
+
+
+(defn record-related-syms
+  ;; hacky 
+  ;; i want to get the symbols related to a record
+  ;; I wouldn't have to do this if there was a way to define a static 
+  ;; method on a record. 
+  [record-sym {:keys [prefix suffix]}]
+  ;; WARNING: hacky code ahead. this feels dirty.
+  (let [parts (-> record-sym
+                  str
+                  (str/split #"\s")
+                  last
+                  (str/split #"\."))
+        record-name (last parts)
+        record-ns (str/join "." (map (fn [s]
+                                       (str/replace s #"_" "-"))  ; replace underscores with dashes
+                                     (butlast parts)))
+        sym (symbol record-ns (str (concat-symbols prefix record-name suffix)))]
+    sym))
+
+(defn spec
+  "Returns the spec for model `modoel`"
+  [model]
+  ((resolve (record-related-syms model {:suffix '-spec}))))
+
+
+(defn construct-record [record-sym]
+  ((resolve (record-related-syms record-sym {:prefix 'map->}))
+   {}))
+
+
+(defmodel foo1
+  {:table :foo1
+   :pk :foo1.a
+   :record
+   {:a int?
+    :b int?}})
+
+(defmodel foo2
+  {:table :foo2
+   :pk :foo2.c
+   :record
+   {:c int?
+    :d int?
+    :foo1 {:model foo1
+           :relationship :one-to-one
+           :fk :a}}})
+
+
+(-> (foo2-spec)
+    :record
+    :foo1
+    :model)
+
+(defn join-table?
+  [column-spec]
+  (and (map? column-spec)
+       (contains? column-spec :relationship)))
+
+
+(defn derived-column?
+  [column-spec]
+  (and (map? column-spec)
+       (contains? column-spec :projection)))
+
+
+(defn one-to-one?
+  [{:keys [relationship]}]
+  (= relationship :one-to-one))
+
+
+(defn one-to-many?
+  [{:keys [relationship]}]
+  (= relationship :one-to-many))
+
+
+(defn many-to-many?
+  [{:keys [relationship]}]
+  (= relationship :many-to-many))
+
+
+(defn qaulified-column
+  [table column]
+  (let [column* (name column)
+        column**  (if (str/includes? column* ".")
+                    (-> column*
+                        (str/split #"\.")
+                        last)
+                    column*)
+        table* (name table)]
+    (keyword (str  table* "." column**))))
+
+(comment
+  (qaulified-column :foo :bar) ; :foo.bar
+  )
+
+(defn model-spec? [{:keys [model] :as m}]
+  (and model (map? m)))
+
+
+(defn join
+  [select-q pk {:keys [model column fk mapping-table-fk mapping-table-pk mapping-table] :as record}]
+  (let [pk* (or column pk)] ; if the child maps to a column in the parent table, that is the column to join the fk on
+    (cond
+      (one-to-one? record) (pg/one-to-one select-q pk* fk)
+      (one-to-many? record) (pg/one-to-many select-q pk* fk)
+      (many-to-many? record) (pg/many-to-many select-q
+                                              pk*
+                                              fk
+                                              mapping-table
+                                              mapping-table-pk
+                                              mapping-table-fk))))
+
+(declare select-query)
+
+(defn qualified-projections
+  [{:keys [table pk record] :as spec}]
+  (reduce-kv (fn [acc k v]
+               (if (join-table? v)
+                 (conj acc [(join (select-query (:model v)) pk v) k])
+                 (conj acc (qaulified-column table k))))
+             []
+             record))
+
+
+(defn select-query [model]
+  (let [{:keys [table record]
+         :as spec} (spec model)]
+    {:select (qualified-projections spec)
+     :from [table]}))
+
+(select-query foo2)
+; {:select
+;  [:foo2.c
+;   :foo2.d
+;   [{:select [[[:to_json :child]]],
+;     :from [[{:select [:foo1.a :foo1.b], :from [:foo1]} :child]],
+;     :where [:= :foo2.c :a]}
+;    :foo1]],
+;  :from [:foo2]}
+
--- test/scribe/core_test.clj
+++ test/scribe/core_test.clj
@@ -1,7 +1,53 @@
 (ns scribe.core-test
   (:require [clojure.test :refer :all]
-            [scribe.core :refer :all]))
+            [scribe.core :refer [record-related-syms select-query defmodel]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(defmodel foo1
+  {:table :foo1
+   :pk :foo1.a
+   :record
+   {:a int?
+    :b int?}})
+
+(defmodel foo2
+  {:table :foo2
+   :pk :foo2.c
+   :record
+   {:c int?
+    :d int?}})
+
+
+(defmodel foo3
+  {:table :foo3
+   :pk :foo3.id
+   :record {:id int?
+            :e int?
+            :foo1 {:model foo1
+                   :relationship :one-to-one
+                   :fk :foo1.a}}})
+
+(deftest test-record-related-syms
+  (is (=  (record-related-syms foo1 {:prefix 'map->})
+          'scribe.core-test/map->foo1))
+  (is (=  (record-related-syms scribe.core_test.foo1 {:prefix 'map->})
+          'scribe.core-test/map->foo1)
+      "Can handle symbols with underscores")
+  (is (=  (record-related-syms scribe.core_test.foo1 {:prefix 'map->
+                                                      :suffix '->vec})
+          'scribe.core-test/map->foo1->vec)
+      "Can handle symbols with underscores"))
+
+(deftest test-select-query
+  (testing "select query generation for one-to-one relationships"
+    (is (= (select-query foo1)
+           {:select [:foo1.a :foo1.b], :from [:foo1]}))
+    (is (= (select-query foo3)
+           {:select
+            [:foo3.id
+             :foo3.e
+             [{:select [[[:to_json :child]]],
+               :from [[{:select [:foo1.a :foo1.b], :from [:foo1]} :child]],
+               :where [:= :foo3.id :foo1.a]}
+              :foo1]],
+            :from [:foo3]}))))
