28d600bc00c10969c9bdb9cc37631ffe15462d7a
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2022-11-16 07:17:53 +0530

    Initialize project

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject scribe "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "MIT"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :repl-options {:init-ns scribe.core})
