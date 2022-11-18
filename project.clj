dbda40a1815717d4217c4b3af4570d535238717f
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2022-11-18 17:04:18 +0530

    add lein eftest, lein auto and cloverage

--- project.clj
+++ project.clj
@@ -4,4 +4,7 @@
   :license {:name "MIT"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
+  :plugins [[lein-auto "0.1.3"]
+            [lein-cloverage "1.0.9"]
+            [lein-eftest "0.5.9"]]
   :repl-options {:init-ns scribe.core})
