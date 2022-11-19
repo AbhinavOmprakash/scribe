a529000693bf7c2bc483cdec49dee999bf7f88fe
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2022-11-19 13:07:32 +0530

    Pre-release snapshot

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject scribe "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject scribe "pre-release-SNAPSHOT"
+  :description "Scribe is an ORM for postgres"
+  :url "https://github.com/AbhinavOmprakash/scribe/blob/master/src/scribe/core.clj"
   :license {:name "MIT"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+            :url "https://github.com/git/git-scm.com/blob/main/MIT-LICENSE.txt"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :plugins [[lein-auto "0.1.3"]
             [lein-cloverage "1.0.9"]
